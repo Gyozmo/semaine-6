bf67f34d51307ebb2c7b2fa7180a8707c645e8ba
Gyozmo <binantguillaume@gmail.com>
2018-05-25 11:38:12 +0200

    json start

---
+++ json exo/script.js
@@ -0,0 +1,27 @@
+// $('document').ready(function (){
+    var resquestURL = 'diapo.json';
+    var request = new XMLHttpRequest();
+    request.open('GET' , resquestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var diapoImgs = request.response;
+        diapoRotate(diapoImgs);
+    }
+
+function diapoRotate(){
+    var resquestURL = 'diapo.json';
+    var request = new XMLHttpRequest();
+    request.open('GET' , resquestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var diapoImgs = request.response;
+    }
+    
+    
+}
+    
+
+
+// })
